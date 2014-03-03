40e25f4fc76d44445b1a761a8fcc25d8a1041ad6
vincent daubry <vdaubry@gmail.com>
2014-03-03 21:22:37 +0100

    test post serializer

---
+++ spec/serializers/post_serializer_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe PostSerializer do
+
+  it "serializes a post" do
+    post = FactoryGirl.create(:post, :id => "5314e4264d6163063f020000", :name => "some_name", :status => "SORTED_STATUS", :pages_url => ["www.foo.bar", "www.foo.bar1"])
+    serializer = PostSerializer.new post
+
+    expect(serializer.to_json).to eql('{"post":{"id":"5314e4264d6163063f020000","name":"some_name","status":"SORTED_STATUS","pages_url":["www.foo.bar","www.foo.bar1"]}}')
+  end
+
+end
