04cd68b6779b91c8e75e608c8884cfb6f8b49fe5
vincent daubry <vdaubry@gmail.com>
2014-03-03 21:09:53 +0100

    api create post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,10 @@
 class PostsController < ApplicationController
-  before_action :set_website, only: [:destroy]
+  before_action :set_website, only: [:destroy, :create]
   before_action :set_post, only: [:destroy]
   respond_to :json
+
+  rescue_from Mongoid::Errors::DocumentNotFound, :with => :render_404
+
 
   def destroy
     @post.images.where(:status => Image::TO_SORT_STATUS).update_all(:status => Image::TO_DELETE_STATUS)
@@ -11,6 +14,13 @@ class PostsController < ApplicationController
     latest_post_id = latest_post.nil? ? nil : latest_post.id.to_s 
     
     render :json => {:latest_post => latest_post_id} 
+  end
+
+  def create
+    post = @website.posts.find_or_create_by(:name => params[:post][:name]) rescue nil
+    respond_with post do |format|
+      format.json { render json: post }
+    end
   end
 
 	private
@@ -27,4 +37,8 @@ class PostsController < ApplicationController
 	def post_params
 	  params.require(:post).permit(:id, :name)
 	end
+
+  def render_404
+    render :status => 404, :text => 'not found'
+  end
 end
