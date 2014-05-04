c76309a82465e040c362907d1fdfc1c805acb916
Vincent Daubry <vdaubry@gmail.com>
2014-05-04 20:44:22 +0200

    diminue la fréquence du nettoyage des posts

--- app/jobs/post_cleaner.rb
+++ app/jobs/post_cleaner.rb
@@ -2,8 +2,10 @@ class PostCleaner
   @queue = :post_clean
 
   def self.perform
+    puts "start cleaning posts"
     post_to_clean_id = Post.to_sort.select {|post| post.images.count == 0}.map(&:id)
     Resque.logger.info "Found #{post_to_clean_id.count} posts to sort"
     Post.where(:_id.in => post_to_clean_id).update_all(:status => Post::SORTED_STATUS)
+    puts "finished cleaning posts"
   end
 end
