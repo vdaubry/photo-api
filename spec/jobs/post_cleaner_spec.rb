dac5ea6fe6326251e84bf7747087341fbdba4803
Vincent Daubry <vdaubry@gmail.com>
2014-08-05 09:37:43 +0200

    correction des tests

--- spec/jobs/post_cleaner_spec.rb
+++ spec/jobs/post_cleaner_spec.rb
@@ -2,17 +2,38 @@ require 'spec_helper'
 
 describe PostCleaner do
   describe "perform" do
-    it "sets all post without images to sorted status" do
-      post_with_images = FactoryGirl.create(:post, :status => Post::TO_SORT_STATUS)
-      post_wihtout_images = FactoryGirl.create(:post, :status => Post::TO_SORT_STATUS)
-      FactoryGirl.create(:image, :post => post_with_images)
-      Post.to_sort.count.should == 2
-      post_with_images.images.count.should == 1
 
-      PostCleaner.perform
+    context "post with images to sort" do
+      it "doesn't change post status" do
+        post_with_images = FactoryGirl.create(:post, :status => Post::TO_SORT_STATUS)
+        FactoryGirl.create(:image, :post => post_with_images, :status => Image::TO_SORT_STATUS)
 
-      Post.to_sort.count.should == 1
-      Post.to_sort.to_a.should == [post_with_images]
+        PostCleaner.perform
+
+        Post.to_sort.count.should == 1
+        Post.to_sort.to_a.should == [post_with_images]
+      end
+    end
+
+    context "post without images" do
+      it "sets to sorted status" do
+        post_without_images = FactoryGirl.create(:post, :status => Post::TO_SORT_STATUS)
+        
+        PostCleaner.perform
+
+        Post.to_sort.count.should == 0
+      end
+    end
+
+    context "post with images already sorted" do
+      it "sets to sorted status" do
+        post_with_images = FactoryGirl.create(:post, :status => Post::TO_SORT_STATUS)
+        FactoryGirl.create(:image, :post => post_with_images, :status => Image::KEPT_STATUS)
+
+        PostCleaner.perform
+
+        Post.to_sort.count.should == 0
+      end
     end
   end
 end
