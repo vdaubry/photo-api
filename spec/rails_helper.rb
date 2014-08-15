683d2dcb0a84c0e57ef1a0f3d8424a4a395511f3
Vincent Daubry <vdaubry@gmail.com>
2014-08-15 20:19:37 +0200

    maj syntax rspec3

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,43 +11,5 @@ Resque.inline = true
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
-# Checks for pending migrations before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
-
-RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  # config.use_transactional_fixtures = true
-
-  # If true, the base class of anonymous controllers will be inferred
-  # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
-  config.infer_base_class_for_anonymous_controllers = false
-
-  #upgrade from rspec-rails 2 to 3 : https://github.com/rspec/rspec-rails/issues/932
-  config.infer_spec_type_from_file_location!
-
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  config.order = "random"
-
-  # Clean/Reset Mongoid DB prior to running each test.
-  config.after(:each) do
-    Mongoid::Sessions.default.collections.select {|c| c.name !~ /system/}.each {|c| c.find.remove_all}
-  end
-end
+#upgrade from rspec-rails 2 to 3 : https://github.com/rspec/rspec-rails/issues/932
+RSpec.configuration.infer_spec_type_from_file_location!
