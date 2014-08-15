e10eca37c23696a186d4d599ae6808986ae9ecbf
Vincent Daubry <vdaubry@gmail.com>
2014-08-15 19:25:12 +0200

    add resque log file + forbidden image hash

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -50,6 +50,12 @@ describe Image do
 				FactoryGirl.build(:image, :source_url => "http://www.foo.bar/img.png").save.should == false
 			end
 		end
+
+		context "forbidden hash" do
+			it "doesn't save image" do
+				FactoryGirl.build(:image, :image_hash => "70bdfc7b6bc66aa8e71cf1915a1cf3fa").save.should == false
+			end
+		end
 	end
 
 	describe "scopes" do
