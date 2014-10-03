4327fbe29cfcbd582c37387fb872594391a17779
Vincent Daubry <vdaubry@gmail.com>
2014-10-03 09:35:00 +0200

    env var in test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+ENV["AWS_ACCESS_KEY"]="toto"
+ENV["AWS_SECRET"]="toto"
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'stub_chain_mocha'
